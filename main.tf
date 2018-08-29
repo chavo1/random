2efd9205c9fa789a3d3ae7046f4dda1205ab3b07
chavo1 <42143239+chavo1@users.noreply.github.com>
2018-08-29 13:53:39 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "random_id" "server" {
+  keepers = {}
+
+  byte_length = 8
+}
+
+resource "null_resource" "server" {
+  provisioner "local-exec" {
+    command = "echo ${random_id.server.hex}"
+  }
+}
+
+resource "random_pet" "server1" {
+  keepers = {}
+}
+
+resource "null_resource" "server1" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.server1.id}"
+  }
+}
